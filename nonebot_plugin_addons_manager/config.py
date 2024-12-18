a2aaa30b3418eb2742082a174ce2bcc6d9c55196
ScorMax <1335900872@qq.com>
2024-12-18 16:00:39 +0800

    添加依赖和README

--- nonebot_plugin_addons_manager/config.py
+++ nonebot_plugin_addons_manager/config.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 from typing import List
 
 class Config(BaseModel):
-    admin_qq:List[int]=[]
+    admin_qq:List[int]=[1111,2222]
     destination_path:str=r"/root/Steam/steamapps/common/Left 4 Dead 2 Dedicated Server/left4dead2/addons"
 
